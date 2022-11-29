4bc54d015437b7240b8ff188ce8d596971702e17
ClaudioGuevaraDev <claudio.guevara.dev@gmail.com>
2022-11-29 10:32:09 -0300

    Flask, react and desktop app configured

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+
+from flask import Flask, render_template
+from flask_cors import CORS
+from flaskwebgui import FlaskUI
+
+app = Flask(
+    __name__,
+    template_folder=os.path.join(os.getcwd(), "frontend", "dist"),
+    static_folder=os.path.join(os.getcwd(), "frontend", "dist", "assets")
+)
+
+CORS(app=app)
+
+
+@app.get("/")
+def index_page():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=4000, debug=True)
+    #FlaskUI(app=app, port=4000, server="flask").run()
+
+# pyinstaller --add-data "frontend/dist;frontend/dist" --noconsole --noconfirm main.py
